3e464d605a8663a43bab27635d35dc7a46f219fc
Marcelo Rodriguez <93072062+j-marcelo-rodriguez@users.noreply.github.com>
2024-02-04 09:58:43 -0400

    default port updated

--- models/server.js
+++ models/server.js
@@ -11,7 +11,7 @@ export class Server {
 
     constructor() {
         this.app = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3000;
         this.usersPath = '/api/users';
 
         this.middlewares();
